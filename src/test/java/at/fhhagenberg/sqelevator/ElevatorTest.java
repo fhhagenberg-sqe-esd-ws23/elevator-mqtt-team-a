cf9e6c41d0da91c3141c06f4f8d0dc77aaf41e2a
Michael Hinterberger <michael-hinterberger@aon.at>
2023-11-15 11:19:16 +0100

    Add capacity changed event test case

--- src/test/java/at/fhhagenberg/sqelevator/ElevatorTest.java
+++ src/test/java/at/fhhagenberg/sqelevator/ElevatorTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import static org.mockito.Mockito.*;
 
+import java.beans.PropertyChangeListener;
 import java.rmi.RemoteException;
 
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -32,6 +33,19 @@ class ElevatorTest {
 		Elevator elevator = new Elevator(plc, 1);
 		
 		assertEquals(2, elevator.getCapacity());
+	}
+	
+	@Test
+	void testCapacityChanged() throws RemoteException {
+		IElevator plc = mock(IElevator.class);
+		when(plc.getElevatorCapacity(0)).thenReturn(4);
+		PropertyChangeListener listener = mock(PropertyChangeListener.class);
+		
+		Elevator elevator = new Elevator(plc, 0);
+		elevator.addPropertyChangeListener(listener);
+		elevator.setCapacity(5);
+		
+		verify(listener, times(1)).propertyChange(argThat(event -> event.getPropertyName() == Elevator.CAPACITY_PROPERTY_NAME && (int)event.getOldValue() == 4 && (int)event.getNewValue() == 5));
 	}
 
 }
